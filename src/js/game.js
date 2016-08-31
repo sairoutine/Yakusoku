223570b242756ea20632bdea08b385b431d8dfb4
sairoutine <sairoutine@gmail.com>
2016-08-31 23:43:47 +0900

    add: game class

--- src/js/game.js
+++ src/js/game.js
@@ -1,4 +1,192 @@
 'use strict';
-var Game;
+
+var config = require('./config');
+var constant = require('./constant');
+/*
+var LoadingScene  = require('./scene/loading');
+var TitleScene    = require('./scene/title');
+var PrologueScene = require('./scene/prologue');
+var StageScene    = require('./scene/stage');
+var EpilogueScene = require('./scene/epilogue');
+var EndingScene   = require('./scene/ending');
+*/
+var Game = function(mainCanvas) {
+	// メインCanvas
+	this.surface = mainCanvas.getContext('2d');
+
+	this.width = Number(mainCanvas.getAttribute('width'));
+	this.height = Number(mainCanvas.getAttribute('height'));
+
+	// ゲームの現在のシーン
+	this.state = null;
+
+	// シーン一覧
+	this.scenes = [];
+	/*
+	// ローディング画面
+	this.scenes[ this.LOADING_SCENE ] = new LoadingScene(this);
+	// オープニング画面
+	this.scenes[ this.OPENING_SCENE ] = new OpeningScene(this);
+	// ゲーム画面
+	this.scenes[ this.STAGE_SCENE ]   = new StageScene(this);
+	// エンディング画面
+	this.scenes[ this.ENDING_SCENE ]  = null;
+	*/
+
+	// 画像一覧
+	this.images = {};
+
+	// SE一覧
+	this.sounds = {};
+
+	// BGM一覧
+	this.bgms = {};
+
+	// どのSEを再生するかのフラグ
+	this.soundflag = 0x00;
+
+	// 経過フレーム数
+	this.frame_count = 0;
+
+	// キー押下フラグ
+	this.keyflag = 0x0;
+
+	// 一つ前のフレームで押下されたキー
+	this.before_keyflag = 0x0;
+};
+
+Game.prototype = {
+	// 初期化
+	init: function () {
+		// 経過フレーム数を初期化
+		this.frame_count = 0;
+
+		// キー押下フラグ
+		this.keyflag = 0x0;
+
+		// 一つ前のフレームで押下されたキー
+		this.before_keyflag = 0x0;
+
+		// シーンをローディング画面にする
+		this.changeScene(constant.LOADING_SCENE);
+	},
+	// キー押下
+	handleKeyDown: function(e){
+		this.keyflag |= this._keyCodeToBitCode(e.keyCode);
+		e.preventDefault();
+	},
+	// キー押下解除
+	handleKeyUp: function(e){
+		this.keyflag &= ~this._keyCodeToBitCode(e.keyCode);
+		e.preventDefault();
+	},
+	// 指定のキーが押下状態か確認する
+	isKeyDown: function(flag) {
+		return this.keyflag & flag;
+	},
+	// 指定のキーが押下されたか確認する
+	isKeyPush: function(flag) {
+		// 1フレーム前に押下されておらず、現フレームで押下されてるなら true
+		return !(this.before_keyflag & flag) && this.keyflag & flag;
+	},
+	// キーコードをBitに変換
+	_keyCodeToBitCode: function(keyCode) {
+		var flag;
+		switch(keyCode) {
+			case 16: // shift
+				flag = constant.BUTTON_SHIFT;
+				break;
+			case 32: // space
+				flag = constant.BUTTON_SPACE;
+				break;
+			case 37: // left
+				flag = constant.BUTTON_LEFT;
+				break;
+			case 38: // up
+				flag = constant.BUTTON_UP;
+				break;
+			case 39: // right
+				flag = constant.BUTTON_RIGHT;
+				break;
+			case 40: // down
+				flag = constant.BUTTON_DOWN;
+				break;
+			case 88: // x
+				flag = constant.BUTTON_X;
+				break;
+			case 90: // z
+				flag = constant.BUTTON_Z;
+				break;
+		}
+		return flag;
+	},
+	// シーンを切り替え
+	changeScene: function(scene) {
+		// シーン切り替え
+		this.state = scene;
+		// 切り替え後のシーンを初期化
+		this.currentScene().init();
+	},
+	// BGMを再生
+	playBGM: function(bgm) {
+		// 全てのBGM再生をストップ
+		for(var key in this.bgms) {
+			this.bgms[key].pause();
+			this.bgms[key].currentTime = 0;
+		}
+
+		// 再生をループする
+		this.bgms[bgm].loop = true;
+		// 再生
+		this.bgms[bgm].play();
+	},
+	// 再生するSEをセット
+	playSound: function(key) {
+		this.soundflag |= config.SOUNDS[key].id;
+	},
+	// セットされたフラグにもとづいてSEを再生
+	runPlaySound: function() {
+		for(var key in config.SOUNDS) {
+			// フラグが立ってたら
+			if(this.soundflag & config.SOUNDS[key].id) {
+				// 再生
+				this.sounds[key].pause();
+				this.sounds[key].currentTime = 0;
+				this.sounds[key].play();
+
+				// フラグを削除
+				this.soundflag &= ~config.SOUNDS[key].id;
+
+				// 1フレームに1つしか再生しない
+				break;
+			}
+		}
+	},
+	// 画像を取得
+	getImage: function(key) {
+		return this.images[key];
+	},
+	currentScene: function() {
+		return this.scenes[this.state];
+	},
+	// ゲーム起動
+	run: function(){
+		// シーン更新
+		this.currentScene().run();
+		this.currentScene().updateDisplay();
+
+		// SEを再生
+		this.runPlaySound();
+
+		// 押下されたキーを保存しておく
+		this.before_keyflag = this.keyflag;
+
+		// 経過フレーム数更新
+		this.frame_count++;
+
+		// 次の描画タイミングで再呼び出ししてループ
+		requestAnimationFrame(this.run.bind(this));
+	},
+};
 
 module.exports = Game;
