6a93cc0a09a0aabdfbc91c50592c5640c078dd79
sairoutine <sairoutine@gmail.com>
2016-09-02 00:12:56 +0900

    compile js

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,489 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
+var Config = {
 
-},{}]},{},[1]);
+
+};
+
+module.exports = Config;
+
+},{}],2:[function(require,module,exports){
+'use strict';
+
+var Constant = {
+	DEBUG: true,
+
+	LOADING_SCENE:  0,
+	TITLE_SCENE:    1,
+	PROLOGUE_SCENE: 2,
+	STAGE_SCENE:    3,
+	EPILOGUE_SCENE: 4,
+	ENDING_SCENE:   5,
+
+	BUTTON_LEFT:  0x01,
+	BUTTON_UP:    0x02,
+	BUTTON_RIGHT: 0x04,
+	BUTTON_DOWN:  0x08,
+	BUTTON_Z:     0x10,
+	BUTTON_X:     0x20,
+	BUTTON_SHIFT: 0x40,
+	BUTTON_SPACE: 0x80,
+
+	IMAGES: {
+		/*
+		title_bg:  'image/title_bg.png',
+		stage1_bg: 'image/stage1_bg.jpg',
+		reimu:     'image/reimu.png',
+		shot:      'image/shot.png',
+		enemy:     'image/enemy.png',
+		bullet:    'image/bullet.png',
+		item:      'image/item.png',
+	   */
+	},
+
+	SOUNDS: {
+		/*
+		select: {
+			id: 0x01,
+			path:   'sound/select.wav',
+			volume: 1.00
+		},
+		shot: {
+			id: 0x02,
+			path: 'sound/shot.wav',
+			volume: 0.08
+		},
+		enemy_vanish: {
+			id: 0x04,
+			path: 'sound/enemy_vanish.wav',
+			volume: 0.1
+		},
+		dead: {
+			id: 0x08,
+			path: 'sound/dead.wav',
+			volume: 0.08
+		},
+		graze: {
+			id: 0x10,
+			path: 'sound/graze.wav',
+			volume: 0.1
+		}
+	   */
+	},
+
+	BGMS:{
+		/*
+		title: {
+			path:   'bgm/title.mp3',
+			volume: 0.40
+		},
+		stage1: {
+			path:   'bgm/stage1.mp3',
+			volume: 0.50
+		},
+	   */
+	},
+};
+
+// 全素材数
+Constant.ALL_MATERIAL_NUM = Object.keys(Constant.IMAGES).length + Object.keys(Constant.SOUNDS).length + Object.keys(Constant.BGMS).length;
+
+
+module.exports = Constant;
+
+},{}],3:[function(require,module,exports){
+'use strict';
+
+var config = require('./config');
+var constant = require('./constant');
+
+var LoadingScene  = require('./scene/loading');
+/*
+var TitleScene    = require('./scene/title');
+var PrologueScene = require('./scene/prologue');
+var StageScene    = require('./scene/stage');
+var EpilogueScene = require('./scene/epilogue');
+var EndingScene   = require('./scene/ending');
+*/
+var Game = function(mainCanvas) {
+	// メインCanvas
+	this.surface = mainCanvas.getContext('2d');
+
+	this.width = Number(mainCanvas.getAttribute('width'));
+	this.height = Number(mainCanvas.getAttribute('height'));
+
+	// ゲームの現在のシーン
+	this.state = null;
+
+	// シーン一覧
+	this.scenes = [];
+	// ローディング画面
+	this.scenes[ constant.LOADING_SCENE ] = new LoadingScene(this);
+	/*
+	// オープニング画面
+	this.scenes[ this.OPENING_SCENE ] = new OpeningScene(this);
+	// ゲーム画面
+	this.scenes[ this.STAGE_SCENE ]   = new StageScene(this);
+	// エンディング画面
+	this.scenes[ this.ENDING_SCENE ]  = null;
+	*/
+
+	// 画像一覧
+	this.images = {};
+
+	// SE一覧
+	this.sounds = {};
+
+	// BGM一覧
+	this.bgms = {};
+
+	// どのSEを再生するかのフラグ
+	this.soundflag = 0x00;
+
+	// 経過フレーム数
+	this.frame_count = 0;
+
+	// キー押下フラグ
+	this.keyflag = 0x0;
+
+	// 一つ前のフレームで押下されたキー
+	this.before_keyflag = 0x0;
+};
+
+Game.prototype = {
+	// 初期化
+	init: function () {
+		// 経過フレーム数を初期化
+		this.frame_count = 0;
+
+		// キー押下フラグ
+		this.keyflag = 0x0;
+
+		// 一つ前のフレームで押下されたキー
+		this.before_keyflag = 0x0;
+
+		// シーンをローディング画面にする
+		this.changeScene(constant.LOADING_SCENE);
+	},
+	// キー押下
+	handleKeyDown: function(e){
+		this.keyflag |= this._keyCodeToBitCode(e.keyCode);
+		e.preventDefault();
+	},
+	// キー押下解除
+	handleKeyUp: function(e){
+		this.keyflag &= ~this._keyCodeToBitCode(e.keyCode);
+		e.preventDefault();
+	},
+	// 指定のキーが押下状態か確認する
+	isKeyDown: function(flag) {
+		return this.keyflag & flag;
+	},
+	// 指定のキーが押下されたか確認する
+	isKeyPush: function(flag) {
+		// 1フレーム前に押下されておらず、現フレームで押下されてるなら true
+		return !(this.before_keyflag & flag) && this.keyflag & flag;
+	},
+	// キーコードをBitに変換
+	_keyCodeToBitCode: function(keyCode) {
+		var flag;
+		switch(keyCode) {
+			case 16: // shift
+				flag = constant.BUTTON_SHIFT;
+				break;
+			case 32: // space
+				flag = constant.BUTTON_SPACE;
+				break;
+			case 37: // left
+				flag = constant.BUTTON_LEFT;
+				break;
+			case 38: // up
+				flag = constant.BUTTON_UP;
+				break;
+			case 39: // right
+				flag = constant.BUTTON_RIGHT;
+				break;
+			case 40: // down
+				flag = constant.BUTTON_DOWN;
+				break;
+			case 88: // x
+				flag = constant.BUTTON_X;
+				break;
+			case 90: // z
+				flag = constant.BUTTON_Z;
+				break;
+		}
+		return flag;
+	},
+	// シーンを切り替え
+	changeScene: function(scene) {
+		// シーン切り替え
+		this.state = scene;
+		// 切り替え後のシーンを初期化
+		this.currentScene().init();
+	},
+	// BGMを再生
+	playBGM: function(bgm) {
+		// 全てのBGM再生をストップ
+		for(var key in this.bgms) {
+			this.bgms[key].pause();
+			this.bgms[key].currentTime = 0;
+		}
+
+		// 再生をループする
+		this.bgms[bgm].loop = true;
+		// 再生
+		this.bgms[bgm].play();
+	},
+	// 再生するSEをセット
+	playSound: function(key) {
+		this.soundflag |= config.SOUNDS[key].id;
+	},
+	// セットされたフラグにもとづいてSEを再生
+	runPlaySound: function() {
+		for(var key in config.SOUNDS) {
+			// フラグが立ってたら
+			if(this.soundflag & config.SOUNDS[key].id) {
+				// 再生
+				this.sounds[key].pause();
+				this.sounds[key].currentTime = 0;
+				this.sounds[key].play();
+
+				// フラグを削除
+				this.soundflag &= ~config.SOUNDS[key].id;
+
+				// 1フレームに1つしか再生しない
+				break;
+			}
+		}
+	},
+	// 画像を取得
+	getImage: function(key) {
+		return this.images[key];
+	},
+	currentScene: function() {
+		return this.scenes[this.state];
+	},
+	// ゲーム起動
+	run: function(){
+		// シーン更新
+		this.currentScene().run();
+		this.currentScene().updateDisplay();
+
+		// SEを再生
+		this.runPlaySound();
+
+		// 押下されたキーを保存しておく
+		this.before_keyflag = this.keyflag;
+
+		// 経過フレーム数更新
+		this.frame_count++;
+
+		// 次の描画タイミングで再呼び出ししてループ
+		requestAnimationFrame(this.run.bind(this));
+	},
+};
+
+module.exports = Game;
+
+},{"./config":1,"./constant":2,"./scene/loading":6}],4:[function(require,module,exports){
+'use strict';
+var Game = require('./game');
+
+window.onload = function() {
+	// Canvas
+	var mainCanvas = document.getElementById('mainCanvas');
+	// Game オブジェクト
+	var game = new Game(mainCanvas);
+	// 初期化
+	game.init();
+	// キーバインド
+	window.onkeydown = function(e) { game.handleKeyDown(e); };
+	window.onkeyup   = function(e) { game.handleKeyUp(e); };
+	// ゲーム起動
+	game.run();
+};
+
+},{"./game":3}],5:[function(require,module,exports){
+'use strict';
+
+/* シーンの基底クラス */
+
+var BaseScene = function(game) {
+	// ゲームインスタンス
+	this.game = game;
+
+	// 経過フレーム数
+	this.frame_count = 0;
+};
+
+// 初期化
+BaseScene.prototype.init = function(){
+	// 経過フレーム数初期化
+	this.frame_count = 0;
+};
+
+// キー押下
+BaseScene.prototype.handleKeyDown = function(e){
+};
+
+// キーを離す
+BaseScene.prototype.handleKeyUp = function(e){
+};
+
+
+// フレーム処理
+BaseScene.prototype.run = function(){
+	// 経過フレーム数更新
+	this.frame_count++;
+
+};
+
+// 画面更新
+BaseScene.prototype.updateDisplay = function(){
+	console.error("updateDisplay method must be overridden");
+};
+
+module.exports = BaseScene;
+
+},{}],6:[function(require,module,exports){
+'use strict';
+
+/* ローディング画面 */
+var Util = require('../util');
+var Constant = require('../constant');
+var BaseScene = require('./base');
+
+var LoadingScene = function(game) {
+	BaseScene.apply(this, arguments);
+
+	// 読み込んだ画像の数
+	this.loadedImageNum = 0;
+	// 読み込んだSEの数
+	this.loadedSoundNum = 0;
+	// 読み込んだBGMの数
+	this.loadedBGMNum = 0;
+};
+
+// 基底クラスを継承
+Util.inherit(LoadingScene, BaseScene);
+
+// 初期化
+LoadingScene.prototype.init = function() {
+	// ゲームで使う画像の読み込み
+	this._loadImages();
+	// SE の読み込み
+	this._loadSounds();
+	// BGM の読み込み
+	this._loadBGMs();
+};
+
+// 読み込んだ素材数
+LoadingScene.prototype.loaded_material_num = function() {
+	return this.loadedImageNum + this.loadedSoundNum + this.loadedBGMNum;
+};
+
+// フレーム処理
+LoadingScene.prototype.run = function(){
+	// 素材を全て読み込んだら
+	if(this.loaded_material_num() >= Constant.ALL_MATERIAL_NUM) {
+		// 読み込み終わったことをゲームに通知
+		//TODO: this.game.notifyLoadingDone();
+	}
+};
+
+// 画面更新
+LoadingScene.prototype.updateDisplay = function(){
+	var material_num = Constant.ALL_MATERIAL_NUM;
+	var loaded_material_num = this.loaded_material_num();
+
+	this.game.surface.save( ) ;
+	this.game.surface.clearRect( 0, 0, this.game.width, this.game.height);
+	this.game.surface.fillStyle = 'rgb( 0, 0, 0 )';
+	this.game.surface.textAlign = 'right';
+	this.game.surface.font = "30px 'ＭＳ ゴシック'";
+	this.game.surface.fillText('Now Loading...', 400, 225);
+	this.game.surface.fillText( loaded_material_num + '/' + material_num, 400, 285);
+	this.game.surface.restore();
+};
+
+LoadingScene.prototype._loadImages = function() {
+	var self = this;
+
+	// 画像が読み込まれたら読み込んだ数を+1
+	var onload_function = function() {
+		self.loadedImageNum++;
+	};
+
+	var image;
+	for(var key in Constant.IMAGES) {
+		image = new Image();
+		image.src = this.game.IMAGES[key] ;
+		image.onload = onload_function;
+		this.game.images[key] = image;
+	}
+
+};
+
+LoadingScene.prototype._loadSounds = function() {
+	var self = this;
+
+	// SEが読み込まれたら読み込んだ数を+1
+	var onload_function = function() {
+		self.loadedBGMNum++;
+	};
+
+	var conf, audio;
+	for(var key in Constant.SOUNDS) {
+		conf = Constant.SOUNDS[key];
+		audio = new Audio(conf.path);
+		audio.volume = conf.volume;
+		audio.addEventListener('canplay', onload_function);
+		audio.load();
+		this.game.sounds[key] = audio;
+	}
+
+};
+
+LoadingScene.prototype._loadBGMs = function() {
+	var self = this;
+
+	// BGMが読み込まれたら読み込んだ数を+1
+	var onload_function = function() {
+		self.loadedBGMNum++;
+	};
+
+	var conf, audio;
+	for(var key in Constant.BGMS) {
+		conf = Constant.BGMS[key];
+		audio = new Audio(conf.path);
+		audio.volume = conf.volume;
+		audio.addEventListener('canplay', onload_function);
+		audio.load();
+		this.game.bgms[key] = audio;
+	}
+};
+
+
+module.exports = LoadingScene;
+
+},{"../constant":2,"../util":7,"./base":5}],7:[function(require,module,exports){
+'use strict';
+var Util = {
+	inherit: function( child, parent ) {
+		var getPrototype = function(p) {
+			if(Object.create) return Object.create(p);
+
+			var F = function() {};
+			F.prototype = p;
+			return new F();
+		};
+		child.prototype = getPrototype(parent.prototype);
+		child.prototype.constructor = child;
+	},
+};
+
+module.exports = Util;
+
+},{}]},{},[4]);
