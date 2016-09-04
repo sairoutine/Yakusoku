f6e524b5acd997a0b4cf5c3c9257c29aae61a897
sairoutine <sairoutine@gmail.com>
2016-09-05 01:04:21 +0900

    add: mock of prologue serif

---
+++ src/js/serif/prologue.js
@@ -0,0 +1,115 @@
+'use strict';
+
+// セリフ
+var Serif= [
+	{
+		serif: "今日は8月31日。",
+		chara: null,
+		pos: null,
+		exp: null,
+	},
+	{
+		serif: null,
+		chara: "ganger",
+		pos: "right",
+		exp: "normal",
+	},
+	{
+		serif: null,
+		chara: "renko",
+		pos: "left",
+		exp: "normal",
+	},
+	{
+		serif: "約束を守りなさい。早く博霊神社に行かないと。",
+		chara: "ganger",
+		pos: "right",
+		exp: "normal",
+	},
+	{
+		serif: "え、そんな約束してたっけ？",
+		chara: "renko",
+		pos: "left",
+		exp: "normal",
+	},
+	{
+		serif: null,
+		chara: "merry",
+		pos: "right",
+		exp: "normal",
+	},
+	{
+		serif: "どうしたの？",
+		chara: "merry",
+		pos: "right",
+		exp: "normal",
+	},
+	{
+		serif: "実はかくかくしかじかで",
+		chara: "renko",
+		pos: "left",
+		exp: "normal",
+	},
+	{
+		serif: "ふーん",
+		chara: "merry",
+		pos: "right",
+		exp: "normal",
+	},
+	{
+		serif: "約束といえば、前に博霊神社の入り口を調べようって約束してたわね。",
+		chara: "renko",
+		pos: "left",
+		exp: "normal",
+	},
+	{
+		serif: "そうだっけ？",
+		chara: "renko",
+		pos: "left",
+		exp: "normal",
+	},
+	{
+		serif: "なんだか気になるわ。ねえ、今から行ってみない？",
+		chara: "renko",
+		pos: "left",
+		exp: "normal",
+	},
+	{
+		serif: "今から！？面倒だわ…。",
+		chara: "merry",
+		pos: "right",
+		exp: "normal",
+	},
+	{
+		serif: "そんな事言わずに行きましょうよ。",
+		chara: "renko",
+		pos: "left",
+		exp: "normal",
+	},
+	{
+		serif: "うっ…急にめまいとフラつきと腹痛と頭痛が",
+		chara: "merry",
+		pos: "right",
+		exp: "normal",
+	},
+	{
+		serif: "さっきまで元気だったじゃない！",
+		chara: "renko",
+		pos: "left",
+		exp: "normal",
+	},
+	{
+		serif: "うーん、気が進まないわ。",
+		chara: "merry",
+		pos: "right",
+		exp: "normal",
+	},
+	{
+		serif: "はぁ…。そんなに嫌なら仕方ないわね。私一人で行ってくるわ。",
+		chara: "renko",
+		pos: "left",
+		exp: "normal",
+	},
+];
+
+module.exports = Serif;
