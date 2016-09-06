514b28345906e0c5f35a5be3f8cf5023baac8207
sairoutine <sairoutine@gmail.com>
2016-09-07 01:52:04 +0900

    compile js

--- public/js/main.js
+++ public/js/main.js
@@ -702,7 +702,7 @@ Scene.prototype.init = function() {
 	BaseScene.prototype.init.apply(this, arguments);
 	this.serif.init();
 
-	this.game.playBGM('prologue');
+	//TODO: this.game.playBGM('prologue');
 };
 
 // フレーム処理
