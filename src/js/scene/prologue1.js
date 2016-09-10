772b13c5c9ac27f5307457983e5ba2bf266cb269
sairoutine <sairoutine@gmail.com>
2016-09-10 21:43:43 +0900

    refactor

--- src/js/scene/prologue1.js
+++ src/js/scene/prologue1.js
@@ -16,6 +16,23 @@ var MESSAGE = (function () {/*
 自分にそっくりな容姿の女の子が立っていた。
 */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
 
+
+// メッセージを表示している期間
+var SHOW_MESSAGE_COUNT = 300;
+
+// メッセージウィンドウの上下左右の余白
+var MESSAGE_WINDOW_OUTLINE_MARGIN = 20;
+var MESSAGE_WINDOW_INLINE_MARGIN  = 50;
+
+// フォントサイズ(px)
+var FONT_SIZE = 24;
+// 行間
+var FONT_MARGIN = 6;
+
+
+
+
+
 // 基底クラス
 var BaseScene = require('./base');
 
@@ -23,7 +40,6 @@ var Util = require('../util');
 var Constant = require('../constant');
 var Config = require('../config');
 
-var SHOW_TITLE_COUNT = 300;
 
 var Scene = function(game) {
 	BaseScene.apply(this, arguments);
@@ -43,63 +59,68 @@ Scene.prototype.init = function() {
 Scene.prototype.run = function(){
 	BaseScene.prototype.run.apply(this, arguments);
 
-	if(SHOW_TITLE_COUNT + 60 < this.frame_count) {
+	// 表示期間終了 or Z押下
+	if(SHOW_MESSAGE_COUNT < this.frame_count ||
+	   this.game.isKeyPush(Constant.BUTTON_Z)) {
 		this.game.notifyPrologue1Done();
 	}
-	/*
-	if(this.game.isKeyPush(Constant.BUTTON_Z)) {
-		if(this.serif.is_end()) {
-			this.game.notifyPrologue1Done();
-		}
-		else {
-			// セリフを送る
-			this.serif.next();
-		}
-	}
-	*/
 };
 
 // 画面更新
 Scene.prototype.updateDisplay = function(){
 	this.game.surface.clearRect( 0, 0, this.game.width, this.game.height ) ;
 
-	var prologue2_bg = this.game.getImage('prologue1_1_bg');
+	var prologue1_bg = this.game.getImage('prologue1_1_bg');
 
 	// 背景画像表示
-	this.game.surface.drawImage(prologue2_bg,
+	this.game.surface.drawImage(prologue1_bg,
 					0,
 					0,
-					prologue2_bg.width,
-					prologue2_bg.height,
+					prologue1_bg.width,
+					prologue1_bg.height,
 					0,
 					0,
 					this.game.width,
 					this.game.height);
 
+
+	// メッセージウィンドウ表示
+	this.game.surface.save();
+
+	this.game.surface.globalAlpha = 0.5;
+	this.game.surface.fillStyle = 'rgb( 0, 0, 0 )';
+	this.game.surface.fillRect(
+		MESSAGE_WINDOW_OUTLINE_MARGIN,
+		MESSAGE_WINDOW_OUTLINE_MARGIN,
+		this.game.width - MESSAGE_WINDOW_OUTLINE_MARGIN * 2,
+		this.game.height - MESSAGE_WINDOW_OUTLINE_MARGIN * 2
+	);
+
+	this.game.surface.restore();
+
+
 	this.game.surface.save();
 
 	var alpha = 1.0;
 	// 切り替え効果
-	if( this.frame_count < (SHOW_TITLE_COUNT / 3)) {
+	if( this.frame_count < (SHOW_MESSAGE_COUNT / 3)) {
 		// 最初の1/3はフェードイン
-		alpha = (this.frame_count * 3) / SHOW_TITLE_COUNT;
+		alpha = (this.frame_count * 3) / SHOW_MESSAGE_COUNT;
 	}
-	else if(SHOW_TITLE_COUNT / 3 < this.frame_count && this.frame_count < SHOW_TITLE_COUNT * 2 / 3) {
+	else if(SHOW_MESSAGE_COUNT / 3 < this.frame_count && this.frame_count < SHOW_MESSAGE_COUNT * 2 / 3) {
 		// 真ん中の1/3は表示
 		alpha = 1.0;
 	}
-	else if(SHOW_TITLE_COUNT * 2 / 3 < this.frame_count) {
+	else if(SHOW_MESSAGE_COUNT * 2 / 3 < this.frame_count) {
 		// 最後の1/3はフェードアウト
-		alpha = (SHOW_TITLE_COUNT - this.frame_count) * 3 / SHOW_TITLE_COUNT;
+		alpha = (SHOW_MESSAGE_COUNT - this.frame_count) * 3 / SHOW_MESSAGE_COUNT;
 	}
 
 	this.game.surface.globalAlpha = alpha;
 
-
-
-
-	if(SHOW_TITLE_COUNT > this.frame_count) {
-		this.game.surface.font = "24px 'Migu'" ;
+	// メッセージ表示期間なら
+	if(SHOW_MESSAGE_COUNT > this.frame_count) {
+		this.game.surface.font = FONT_SIZE + "px 'Migu'" ;
 		this.game.surface.textBaseAlign = 'middle' ;
 		this.game.surface.fillStyle = 'rgb( 255, 255, 255 )' ;
 
@@ -107,12 +128,12 @@ Scene.prototype.updateDisplay = function(){
 		var lines = MESSAGE.split("\n");
 		if (lines.length) {
 			// セリフテキストの y 座標初期位置
-			var y = 50;
+			var y = MESSAGE_WINDOW_INLINE_MARGIN;
 
 			for(var i = 0, len = lines.length; i < len; i++) {
-				this.game.surface.fillText(lines[i], 50, y); // 1行表示
+				this.game.surface.fillText(lines[i], MESSAGE_WINDOW_INLINE_MARGIN, y); // 1行表示
 
-				y+= 30;
+				y+= FONT_SIZE + FONT_MARGIN;
 			}
 		}
 	}
