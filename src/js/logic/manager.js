0f49e3d6ac79cb4e530051e19980e6c36c5e1eb7
sairoutine <sairoutine@gmail.com>
2016-09-14 02:05:52 +0900

    add: logic/manager class

---
+++ src/js/logic/manager.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var Factory = require("./factory");
+
+var Manager = function(Class, stage) {
+	// StageScene インスタンス
+	this.stage = stage;
+
+	// オブジェクト生成クラス
+	this.factory = new Factory(Class);
+
+	// 画面上のオブジェクト一覧
+	this.objects = {};
+
+	// フレーム数
+	this.frame_count = 0;
+};
+
+// 初期化
+Manager.prototype.init = function() {
+	// 画面上のオブジェクト一覧
+	this.objects = {};
+
+	// フレーム数
+	this.frame_count = 0;
+};
+
+// オブジェクト生成
+Manager.prototype.create = function() {
+	var obj = this.factory.get();
+
+	this.objects[obj.id] = obj;
+
+	return obj;
+};
+
+// オブジェクト削除
+Manager.prototype.remove = function(id) {
+	delete this.objects[id];
+
+	this.factory.free(id);
+};
+
+// フレーム処理
+Manager.prototype.run = function(){
+	this.frame_count++;
+
+	// オブジェクト一覧
+	for(var id in this.objects) {
+		this.objects[id].run();
+	}
+
+	// 画面外に出たオブジェクトを削除
+	this.removeOutOfStageObjects();
+};
+
+// 画面外に出たオブジェクトを消去する
+Manager.prototype.removeOutOfStageObjects = function() {
+	// オブジェクトが画面外に出たかどうか判定
+	for(var id in this.objects) {
+		if(this.objects[id].isOutOfStage()) {
+			this.remove(id);
+		}
+	}
+};
+
+// 画面更新
+Manager.prototype.updateDisplay = function(){
+	// オブジェクト一覧
+	for(var id in this.objects) {
+		this.objects[id].updateDisplay();
+	}
+
+};
+
+module.exports = Manager;
