497408bc716767be6f713ae3832471d601cd5687
sairoutine <sairoutine@gmail.com>
2016-09-17 22:55:08 +0900

    add: mock of aya serif

---
+++ src/js/serif/stage1.js
@@ -0,0 +1,65 @@
+'use strict';
+
+// セリフ
+var Serif= [
+	{
+		serif: null,
+		fukidashi: null,
+		chara: "aya",
+		pos: "right",
+		exp: "normal",
+	},
+	{
+		serif: null,
+		fukidashi: null,
+		chara: "renko",
+		pos: "left",
+		exp: "normal",
+	},
+	{
+
+		serif: "なんだ、ただの迷子ですか。\n記事にもなりませんね。",
+		fukidashi: "normal",
+		chara: "aya",
+		pos: "right",
+		exp: "normal",
+	},
+	{
+		serif: "博麗神社に行きたいの。",
+		fukidashi: "normal",
+		chara: "renko",
+		pos: "left",
+		exp: "normal",
+	},
+	{
+		serif: "神社？この道をダーって行って\nキュって曲がってガーって\n進んでギャーンで直ぐですよ",
+		fukidashi: "normal",
+		chara: "aya",
+		pos: "right",
+		exp: "normal",
+	},
+	{
+		serif: "ダー！キュっ！ガーっ！\nギャーン！ね。ありがとう！",
+		fukidashi: "normal",
+		chara: "renko",
+		pos: "left",
+		exp: "normal",
+	},
+	{
+		serif: "その前に私と一戦",
+		fukidashi: "normal",
+		chara: "aya",
+		pos: "right",
+		exp: "normal",
+	},
+	{
+		serif: "あら？",
+		fukidashi: "normal",
+		chara: "renko",
+		pos: "left",
+		exp: "normal",
+	},
+
+];
+
+module.exports = Serif;
