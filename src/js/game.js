b28749f1c82c79077f1463843f4ae7cf4e8be323
sairoutine <sairoutine@gmail.com>
2016-09-17 18:12:34 +0900

    fix: safari bgm start bug

--- src/js/game.js
+++ src/js/game.js
@@ -169,7 +169,7 @@ Game.prototype = {
 		source.connect(self.context.destination);
 		source.start = source.start || source.noteOn;
 		source.stop  = source.stop  || source.noteOff;
-		source.start();
+		source.start(0);
 
 		// cache
 	},
