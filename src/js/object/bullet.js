a80afb8fb4a2e73f32a485f572b038a3e3b441ae
sairoutine <sairoutine@gmail.com>
2016-09-24 02:20:53 +0900

    rename class name bullet object -> bullet

--- src/js/object/bullet.js
+++ src/js/object/bullet.js
@@ -7,14 +7,14 @@ var VectorBaseObject = require('./vector_base');
 var Util = require('../util');
 var Constant = require('../constant');
 
-var BulletObject = function(scene) {
+var Bullet = function(scene) {
 	VectorBaseObject.apply(this, arguments);
 	// 敵弾のスプライト上の位置
 	this.indexX = 0; this.indexY = 0;
 };
-Util.inherit(BulletObject, VectorBaseObject);
+Util.inherit(Bullet, VectorBaseObject);
 
-BulletObject.prototype.init = function(x, y, r, theta, sprite_x, sprite_y) {
+Bullet.prototype.init = function(x, y, r, theta, sprite_x, sprite_y) {
 	// TODO: リファクタ
 	VectorBaseObject.prototype.init.apply(this, [
 		[
@@ -32,27 +32,27 @@ BulletObject.prototype.init = function(x, y, r, theta, sprite_x, sprite_y) {
 	this.indexX = sprite_x || 0;
 	this.indexY = sprite_y || 0;
 };
-BulletObject.prototype.run = function() {
+Bullet.prototype.run = function() {
 	// ベクトルに従って移動
 	VectorBaseObject.prototype.run.apply(this, arguments);
 };
 
 // 当たり判定サイズ
-BulletObject.prototype.collisionWidth  = function() { return 13; };
-BulletObject.prototype.collisionHeight = function() { return 20; };
+Bullet.prototype.collisionWidth  = function() { return 13; };
+Bullet.prototype.collisionHeight = function() { return 20; };
 
 // スプライトの開始位置
-BulletObject.prototype.spriteX = function() { return this.indexX; };
-BulletObject.prototype.spriteY = function() { return this.indexY; };
+Bullet.prototype.spriteX = function() { return this.indexX; };
+Bullet.prototype.spriteY = function() { return this.indexY; };
 
 // スプライト画像
-BulletObject.prototype.spriteImage = function() { return 'shot'; };
+Bullet.prototype.spriteImage = function() { return 'shot'; };
 
 // スプライトのサイズ
-BulletObject.prototype.spriteWidth  = function() { return 13; };
-BulletObject.prototype.spriteHeight = function() { return 20; };
+Bullet.prototype.spriteWidth  = function() { return 13; };
+Bullet.prototype.spriteHeight = function() { return 20; };
 
 
 
 
-module.exports = BulletObject;
+module.exports = Bullet;
