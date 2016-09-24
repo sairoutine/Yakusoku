61051249c2f4642c58e89051bba57b204253eae9
sairoutine <sairoutine@gmail.com>
2016-09-24 18:46:42 +0900

    to use bullet type

--- src/js/object/bullet.js
+++ src/js/object/bullet.js
@@ -7,30 +7,44 @@ var VectorBaseObject = require('./vector_base');
 var Util = require('../util');
 var Constant = require('../constant');
 
+// 弾の種類
+var bullet_types = require("../enemy/bullet_types");
+
+
+
 var Bullet = function(scene) {
 	VectorBaseObject.apply(this, arguments);
-	// 敵弾のスプライト上の位置
-	this.indexX = 0; this.indexY = 0;
 };
+
 Util.inherit(Bullet, VectorBaseObject);
 
-Bullet.prototype.init = function(x, y, r, theta, sprite_x, sprite_y) {
+Bullet.prototype.init = function(type_id, x, y, vector) {
+	// VectorBaseObject.init より先に設定しないと aim が効かない
+	this.x = x;
+	this.y = y;
+
+	var type = bullet_types[type_id];
 	// TODO: リファクタ
 	VectorBaseObject.prototype.init.apply(this, [
 		[
 			{
 				count: 0,
-				vector: {r: r, theta: theta},
-				is_rotate: true,
+				vector: vector,
+				is_rotate: type.is_rotate,
 			}
 		]
 	]);
-	this.x = x;
-	this.y = y;
-
-	// TODO:
-	this.indexX = sprite_x || 0;
-	this.indexY = sprite_y || 0;
+	// 画像の種類
+	this.image            = type.image;
+	// スプライト開始位置
+	this.indexX           = type.indexX;
+	this.indexY           = type.indexY;
+	// スプライトサイズ
+	this.width            = type.width;
+	this.height           = type.height;
+	// 当たり判定サイズ
+	this.collision_width  = type.collision_width;
+	this.collision_height = type.collision_height;
 };
 Bullet.prototype.run = function() {
 	// ベクトルに従って移動
@@ -38,21 +52,18 @@ Bullet.prototype.run = function() {
 };
 
 // 当たり判定サイズ
-Bullet.prototype.collisionWidth  = function() { return 13; };
-Bullet.prototype.collisionHeight = function() { return 20; };
+Bullet.prototype.collisionWidth  = function() { return this.collision_width; };
+Bullet.prototype.collisionHeight = function() { return this.collision_height; };
 
 // スプライトの開始位置
 Bullet.prototype.spriteX = function() { return this.indexX; };
 Bullet.prototype.spriteY = function() { return this.indexY; };
 
 // スプライト画像
-Bullet.prototype.spriteImage = function() { return 'shot'; };
+Bullet.prototype.spriteImage = function() { return this.image; };
 
 // スプライトのサイズ
-Bullet.prototype.spriteWidth  = function() { return 13; };
-Bullet.prototype.spriteHeight = function() { return 20; };
-
-
-
+Bullet.prototype.spriteWidth  = function() { return this.width; };
+Bullet.prototype.spriteHeight = function() { return this.height; };
 
 module.exports = Bullet;
