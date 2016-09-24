69d87dd407f7fa4c8d1b373a2bd2a038ea49586b
sairoutine <sairoutine@gmail.com>
2016-09-24 18:45:20 +0900

    ad: bullet dictionary

---
+++ src/js/enemy/bullet_dictionaries.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var BulletDictionaries = [
+	[
+		{
+			'vector': { 'r': 5, 'theta': 165, 'w': 0, 'ra': 0.1, 'wa': 0, 'raa': -0.01, 'rrange': { 'min': 2 } },
+		},
+		{
+			'vector': { 'r': 5, 'theta': 135, 'w': 0, 'ra': 0.1, 'wa': 0, 'raa': -0.01, 'rrange': { 'min': 2 } },
+		},
+		{
+			'vector': { 'r': 5, 'theta': 105, 'w': 0, 'ra': 0.1, 'wa': 0, 'raa': -0.01, 'rrange': { 'min': 2 } },
+		},
+		{
+			'vector': { 'r': 5, 'theta':  75, 'w': 0, 'ra': 0.1, 'wa': 0, 'raa': -0.01, 'rrange': { 'min': 2 } },
+		},
+		{
+			'vector': { 'r': 5, 'theta':  45, 'w': 0, 'ra': 0.1, 'wa': 0, 'raa': -0.01, 'rrange': { 'min': 2 } },
+		},
+		{
+			'vector': { 'r': 5, 'theta':  15, 'w': 0, 'ra': 0.1, 'wa': 0, 'raa': -0.01, 'rrange': { 'min': 2 } },
+		},
+	],
+	[
+		{
+			'vector': { 'aimed': true, 'r': 5, 'theta':  0, 'w': 0, 'ra': 0.1, 'wa': 0, 'raa': -0.01, 'rrange': { 'min': 3 } },
+		},
+	],
+];
+
+module.exports = BulletDictionaries;
