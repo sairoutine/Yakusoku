453bfa1adda893ccd5b6ffe616a95a6bc756774b
sairoutine <sairoutine@gmail.com>
2016-09-25 17:52:36 +0900

    add: spell card 1

--- src/js/spell/stage1/tengukaze.js
+++ src/js/spell/stage1/tengukaze.js
@@ -8,16 +8,11 @@ var Constant = require('../../constant');
 
 var Spell = function(boss) {
 	BaseSpell.apply(this, arguments);
-	this.frame_count = 0;
-	this.shot_thetas1 = [0, 60, 120, 180, 240, 300];
-	this.shot_thetas2 = [0, 60, 120, 180, 240, 300];
 	this.maru_shot_theta = 0;
 
 	// config
-	this.add_shot_theta = 5;
 	this.r = 1.5;
-	this.uzumaki_percount = 5;
-	this.maru_percount    = 75;
+	this.maru_percount1    = 100;
 	this.maru_shot_pertheta = 10;
 
 };
@@ -33,22 +28,6 @@ Spell.prototype.run = function() {
 
 	// スペルカード発動演出中
 	if(this.isSpellStarting()) return;
-
-	// 渦巻き弾
-	if(this.frame_count % this.uzumaki_percount === 0) {
-		this.uzumaki_shot1();
-		this.uzumaki_shot2();
-		this.game.playSound('boss_shot_small');
-	}
-
-	// 円形弾
-	if(this.frame_count % this.maru_percount === 0) {
-		for (var i=0; i< 360 / this.maru_shot_pertheta; i++) {
-			this.maru_shot();
-			this.maru_shot_theta += this.maru_shot_pertheta;
-		}
-		this.game.playSound('boss_shot_big');
-	}
 
 	// 移動
 	var shot_time = 600;
@@ -72,43 +51,55 @@ Spell.prototype.run = function() {
 		this.boss.animateLeft();
 	}
 	else {
+		// 移動してない時
+
+		// 円形弾
+		if(
+			this.frame_count % this.maru_percount1 === 0  ||
+			this.frame_count % this.maru_percount1 === 10 ||
+			this.frame_count % this.maru_percount1 === 20
+		) {
+			// 自機狙い
+			this.aimedToChara();
+			for (var i=0; i< 360 / this.maru_shot_pertheta; i++) {
+				this.maru_shot();
+				this.maru_shot_theta += this.maru_shot_pertheta;
+			}
+			this.game.playSound('boss_shot_big');
+		}
+
 		this.boss.animateNeutral();
 	}
 
 };
 
-Spell.prototype.uzumaki_shot1 = function() {
-	var x = this.boss.x;
-	var y = this.boss.y;
-	var r = this.r;
-
-	for(var i = 0; i < this.shot_thetas1.length; i++ ) {
-		var theta = this.shot_thetas1[i];
-
-		this.shot(0, x, y, {r: r, theta: theta}); // type_id: 0
-		this.shot_thetas1[i] += this.add_shot_theta;
-	}
-};
-Spell.prototype.uzumaki_shot2 = function() {
-	var x = this.boss.x;
-	var y = this.boss.y;
-	var r = this.r;
-
-	for(var i = 0; i < this.shot_thetas2.length; i++ ) {
-		var theta = this.shot_thetas2[i];
-
-		this.shot(0, x, y, {r: r, theta: theta}); // type_id: 0
-		this.shot_thetas2[i] -= this.add_shot_theta;
-	}
-};
 Spell.prototype.maru_shot = function() {
 	var x = this.boss.x;
 	var y = this.boss.y;
 	var theta = this.maru_shot_theta;
 	var r = this.r;
 
-	this.shot(1, x, y, {r: r, theta: theta}); // type_id: 1
+	this.shot(3, x, y, {r: r, theta: theta}); // type_id: 1
 };
+
+// 自機狙いにする
+Spell.prototype.aimedToChara = function() {
+	// 自機
+	var character = this.stage.character;
+
+	var ax = character.x - this.boss.x;
+	var ay = character.y - this.boss.y;
+
+	this.maru_shot_theta = this._radian_to_theta(Math.atan2(ay, ax));
+};
+
+// ラジアン -> θ に変換
+Spell.prototype._radian_to_theta = function(radian) {
+	return (radian * 180 / Math.PI) | 0;
+};
+
+
+
 
 Spell.prototype.name = function() { return "風符「天狗風」"; };
 
