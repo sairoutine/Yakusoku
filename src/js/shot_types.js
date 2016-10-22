da203f8a3b85a5e609598f07852a8b5602e907f5
sairoutine <sairoutine@gmail.com>
2016-10-22 22:11:25 +0900

    add: shot_types config

---
+++ src/js/shot_types.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var ShotTypes = [
+	{
+		'image':       'shot',
+		'indexX':           0,
+		'indexY':           2,
+		'width':           20,
+		'height':          20,
+		'collisionWidth':  15,
+		'collisionHeight': 15,
+		'is_rotate':       false,
+		'vector': { 'r': 8, 'theta': 270 },
+	},
+];
+
+module.exports = ShotTypes;
