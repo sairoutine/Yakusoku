06599e6df1bc24ddc4ad0422f4704726f988de7a
sairoutine <sairoutine@gmail.com>
2016-11-14 02:21:14 +0900

    update: option shots

--- src/js/object/option.js
+++ src/js/object/option.js
@@ -5,6 +5,15 @@
 // 基底クラス
 var BaseObject = require('./base');
 var Util = require('../util');
+var Constant = require('../constant');
+
+// 自機弾のベクトル
+var SHOT_VECTOR = { 'r': 8, 'theta': 270 };
+// Nフレーム毎に弾を撃つ
+var SHOT_SPAN_1 = 50;
+var SHOT_SPAN_2 = 5;
+
+
 
 var Option = function(id, scene) {
 	BaseObject.apply(this, arguments);
@@ -19,6 +28,7 @@ Option.prototype.setCharacterPosition = function() {
 };
 
 Option.prototype.init = function(character, x, y) {
+	BaseObject.prototype.init.apply(this, arguments);
 	this.character = character;
 	this.local_x = x;
 	this.local_y = y;
@@ -27,9 +37,17 @@ Option.prototype.init = function(character, x, y) {
 };
 
 Option.prototype.run = function() {
-	this.setCharacterPosition();
 	BaseObject.prototype.run.apply(this, arguments);
+	// 自機の周辺に移動
+	this.setCharacterPosition();
 
+	// レベルアップすると弾を撃つ感覚が速くなる
+	var span = this.character.level < 4 ? SHOT_SPAN_1 : SHOT_SPAN_2;
+
+	// 弾を撃つ
+	if(this.frame_count % span === 0) {
+		this.stage.shot_manager.create(Constant.SHOT_OPTION_TYPE, this.x, this.y, SHOT_VECTOR);
+	}
 };
 
 // スプライトの開始位置
