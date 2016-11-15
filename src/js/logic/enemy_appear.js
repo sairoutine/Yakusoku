bf788b7f92d6c4dc6a08edd926b3bebdaeaf5436
sairoutine <sairoutine@gmail.com>
2016-11-16 01:50:51 +0900

    add: getLastEnemyAppearCount method in enemy_appear logic

--- src/js/logic/enemy_appear.js
+++ src/js/logic/enemy_appear.js
@@ -16,7 +16,7 @@ Logic.prototype.init = function() {
 Logic.prototype.get = function(frame_count) {
 	var params = [];
 	// 現在フレームに出現予定の敵を出現させる
-	while(this.appear_params[this.enemy_index] &&
+	while(!this.isEnd() &&
 		this.appear_params[this.enemy_index].appear_frame === frame_count) {
 
 		params.push(this.appear_params[this.enemy_index]);
@@ -26,7 +26,13 @@ Logic.prototype.get = function(frame_count) {
 	return params;
 };
 
+// 敵生成が全て終了したか
+Logic.prototype.isEnd = function() {
+	return this.appear_params[this.enemy_index] ? false : true;
+};
 
-
-
+// 最後に敵が出現した際のフレームを取得
+Logic.prototype.getLastEnemyAppearCount = function() {
+	return this.appear_params[this.appear_params.length-1].appear_frame;
+};
 module.exports = Logic;
