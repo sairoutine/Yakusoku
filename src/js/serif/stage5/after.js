ab5897a7c4b3a9b863afb8a5a926e6b3aecb1176
sairoutine <sairoutine@gmail.com>
2016-11-16 02:25:16 +0900

    add: mock of serif info

---
+++ src/js/serif/stage5/after.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// セリフ
+var Serif= [{"pos":"left","exp":"normal","chara":"renko","fukidashi":"normal","serif":null},{"pos":"right","exp":"dissatisfied","chara":"aya","fukidashi":"normal","serif":"なんだ、ただの迷子ですか。\n記事にもなりませんね。"},{"pos":"left","exp":"calm","chara":"renko","fukidashi":"normal","serif":"博麗神社に行きたいの。"},{"pos":"right","exp":"normal","chara":"aya","fukidashi":"normal","serif":"神社？"},{"pos":"right","exp":"smile","chara":"aya","fukidashi":"orange","serif":"この道をダーって行って\nキュって曲がってギャーン\nって行けばすぐですよ。"},{"pos":"left","exp":"normal","chara":"renko","fukidashi":"orange","serif":"この道をダーって行って\nキュって曲がってギャーン\nね。ありがとう！"}];
+
+module.exports = Serif;
