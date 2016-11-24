61ad3d4c3a6d71a12e3ae91e93aafed867424b4f
sairoutine <sairoutine@gmail.com>
2016-11-25 00:33:17 +0900

    update: sanae

--- src/js/object/boss/sanae.js
+++ src/js/object/boss/sanae.js
@@ -1,15 +1,16 @@
 'use strict';
 
-/* ステージ2ボス 東風谷早苗 */
+/* ステージ1ボス 射命丸文 */
 
 // 基底クラス
 var BaseObject = require('../base');
 var Util = require('../../util');
 var Constant = require('../../constant');
 
-
-var Spell1 = require('../../spell/stage1/spell1');
-var Spell2 = require('../../spell/stage1/spell2');
+var Spell2 = require('../../spell/stage2/spell2');
+var Spell4 = require('../../spell/stage2/spell4');
+var Spell5 = require('../../spell/stage2/spell5');
+var Spell7 = require('../../spell/stage2/spell7');
 
 
 var Shot = require('../../object/shot');
@@ -26,7 +27,7 @@ var LR_ANIMATION_SPAN = 4;
 var VITAL = 60 * 60 * 1;
 
 // ボスの移動速度
-var SPEED = 2;
+var DEFAULT_SPEED = 2;
 
 // constructor
 var Aya = function(stage) {
@@ -42,26 +43,33 @@ var Aya = function(stage) {
 	// スペルカード一覧
 	this.spells = [
 		null, // 何も発動していない
-		new Spell1(this),
-		new Spell2(this),
+		new Spell4(this), // 簡単 文じゃなくてもよい
+		new Spell5(this), // 星。早苗さん用
+
+		//new Spell7(this), // 文あるいは早苗じゃなくても良い どはで
+		//new Spell2(this),// 文あるいは早苗じゃなくてもよい。結構どはで
 	];
+
+	// 移動関連
+	this.is_moving = false;// 移動中か否か
+	this.to_x      = null; // 移動先 x座標
+	this.to_y      = null; // 移動先 y座標
+	this.to_radian = null; // 移動方向
+	this.to_speed  = null; // 移動速度
+
+	// ボスを描画するかどうか
+	this.is_show = true;
 };
 
 // 基底クラスを継承
 Util.inherit(Aya, BaseObject);
 
-
-
-// ボスを初期位置に置く
-Aya.prototype.setInitPosition = function() {
-	// ボスの初期位置
-	this.x = (this.stage.width / 2);
-	this.y = (this.stage.height - 400);
-};
-
 // 初期化
 Aya.prototype.init = function() {
 	BaseObject.prototype.init.apply(this, arguments);
+
+	// 移動を初期化
+	this.clearMoveTo();
 
 	// ボスを初期位置に置く
 	this.setInitPosition();
@@ -78,6 +86,16 @@ Aya.prototype.init = function() {
 
 	// ボス出現エフェクト
 	this.boss_appearance = new CreateJS(new boss_appearance.boss_appearance(), 960, 960);
+
+	// ボスを描画するかどうか
+	this.is_show = true;
+};
+
+// ボスを初期位置に置く
+Aya.prototype.setInitPosition = function() {
+	// ボスの初期位置
+	this.x = (this.stage.width / 2);
+	this.y = (this.stage.height - 400);
 };
 
 // 現在のスペルカード
@@ -117,8 +135,12 @@ Aya.prototype.run = function(){
 	// スペルカード処理
 	this.currentSpell().run();
 
-	// 時間経過でスペルカード発動時間は減っていく
+	// 移動が設定されてるなら移動
+	this._moveTo();
+
+	// スペルカード実行中ならば
 	if(this.currentSpell().isSpellExecute()) {
+		// 時間経過でスペルカード発動時間は減っていく
 		this.vital--;
 		this.stage.score+=10;
 	}
@@ -146,44 +168,17 @@ Aya.prototype.run = function(){
 
 // 移動
 Aya.prototype.moveLeft = function(){
-	this.x -= SPEED;
+	this.x -= DEFAULT_SPEED;
 };
 Aya.prototype.moveRight = function(){
-	this.x += SPEED;
+	this.x += DEFAULT_SPEED;
 };
 Aya.prototype.moveUp = function(){
-	this.y -= SPEED;
+	this.y -= DEFAULT_SPEED;
 };
 Aya.prototype.moveDown = function(){
-	this.y += SPEED;
+	this.y += DEFAULT_SPEED;
 };
-
-// TODO: refactor
-// theta値の方向に移動
-Aya.prototype.moveByTheta = function(theta){
-	this.x += this.calc_moveX(theta);
-	this.y += this.calc_moveY(theta);
-};
-
-// θ -> ラジアンに変換
-Aya.prototype._theta_to_radian = function(theta){
-	return (theta / 180 * Math.PI);
-};
-
-// X軸の移動を計算
-Aya.prototype.calc_moveX = function(theta) {
-	var move_x = SPEED * Math.cos(this._theta_to_radian(theta));
-	return move_x;
-};
-
-// Y軸の移動を計算
-Aya.prototype.calc_moveY = function(theta) {
-	var move_y = SPEED * Math.sin(this._theta_to_radian(theta));
-	return move_y;
-} ;
-
-
-
 
 // 移動アニメーション
 Aya.prototype.animateLeft = function(){
@@ -197,18 +192,77 @@ Aya.prototype.animateNeutral = function(){
 };
 
 
+// 移動中かどうか
+Aya.prototype.isMoving = function(){
+	return this.is_moving;
+};
+// 指定のx,y座標に移動を設定
+Aya.prototype.setMoveTo = function(x, y, frame_count){
+	this.is_moving = true;
+	this.to_x = x;
+	this.to_y = y;
+
+	var ax = x - this.x;
+	var ay = y - this.y;
+
+	this.to_radian = Math.atan2(ay, ax);
+	if(frame_count) {
+		this.to_speed = Math.sqrt(Math.pow(ay, 2) + Math.pow(ax, 2)) / frame_count;
+	}
+	else {
+		this.to_speed = DEFAULT_SPEED;
+	}
+};
+// 指定の座標に移動しているのを解除
+Aya.prototype.clearMoveTo = function(){
+	this.is_moving = false;
+	this.to_x = null;
+	this.to_y = null;
+	this.to_radian = null;
+	this.to_speed = null;
+};
+
+
+// 指定のx,y座標に移動
+Aya.prototype._moveTo = function(){
+	if(!this.is_moving) return;
+
+	var cos = Math.cos(this.to_radian);
+	var sin = Math.sin(this.to_radian);
+
+	this.x += this.to_speed * cos;
+	this.y += this.to_speed * sin;
+
+	if(cos > 0) {
+		this.animateRight();
+	}
+	else {
+		this.animateLeft();
+	}
+
+	if( this.to_x + 1 > this.x && this.x > this.to_x - 1 &&
+		this.to_y + 1 > this.y && this.y > this.to_y - 1) {
+		this.clearMoveTo();
+		this.animateNeutral();
+	}
+};
+
+
+
 // ボスを描画
 Aya.prototype.updateDisplay = function(){
 	var ctx = this.game.surface;
 
-	// ボス出現エフェクト
-	ctx.save();
-	ctx.translate(this.x, this.y);
-	ctx.drawImage(this.boss_appearance.canvas, (-this.boss_appearance.canvas.width/2), (-this.boss_appearance.canvas.height/2));
-	ctx.restore();
+	if(this.is_show) {
+		// ボス出現エフェクト
+		ctx.save();
+		ctx.translate(this.x, this.y);
+		ctx.drawImage(this.boss_appearance.canvas, (-this.boss_appearance.canvas.width/2), (-this.boss_appearance.canvas.height/2));
+		ctx.restore();
 
-	// ボス描画
-	BaseObject.prototype.updateDisplay.apply(this, arguments);
+		// ボス描画
+		BaseObject.prototype.updateDisplay.apply(this, arguments);
+	}
 
 	// スペルカード描画
 	this.currentSpell().updateDisplay();
@@ -242,8 +296,6 @@ Aya.prototype.spriteHeight = function() { return 128; };
 
 // BGM
 Aya.prototype.bgm = function() { return 'stage1'; };
-
-
 
 
 module.exports = Aya;
