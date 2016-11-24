f788689c0cccace1037c81cc75cbf0a3bd95659f
sairoutine <sairoutine@gmail.com>
2016-11-25 00:32:49 +0900

    update: aya's spell

--- src/js/object/boss/aya.js
+++ src/js/object/boss/aya.js
@@ -8,12 +8,8 @@ var Util = require('../../util');
 var Constant = require('../../constant');
 
 var Spell1 = require('../../spell/stage1/spell1');
-var Spell2 = require('../../spell/stage1/spell2');
 var Spell3 = require('../../spell/stage1/spell3');
-var Spell4 = require('../../spell/stage1/spell4');
-var Spell5 = require('../../spell/stage1/spell5');
 var Spell6 = require('../../spell/stage1/spell6');
-var Spell7 = require('../../spell/stage1/spell7');
 var Spell8 = require('../../spell/stage1/spell8');
 
 
@@ -47,14 +43,10 @@ var Aya = function(stage) {
 	// スペルカード一覧
 	this.spells = [
 		null, // 何も発動していない
-		new Spell8(this),
 		new Spell1(this),
-		new Spell2(this),
+		new Spell8(this),
 		new Spell3(this),
-		new Spell4(this),
-		new Spell5(this),
 		new Spell6(this),
-		new Spell7(this),
 	];
 
 	// 移動関連
