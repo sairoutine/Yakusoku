f2c33f946456e3bd78bd5781927f04c402ce91bd
sairoutine <sairoutine@gmail.com>
2016-12-02 21:18:31 +0900

    update spell2 of stage2

--- src/js/spell/stage2/spell2.js
+++ src/js/spell/stage2/spell2.js
@@ -7,16 +7,6 @@ var Constant = require('../../constant');
 
 var Spell = function(boss) {
 	BaseSpell.apply(this, arguments);
-	this.shot_thetas1 = [0, 60, 120, 180, 240, 300];
-	this.shot_thetas2 = [0, 60, 120, 180, 240, 300];
-	this.maru_shot_theta = 0;
-
-	// config
-	this.add_shot_theta = 10;
-	this.r = 1.5;
-	this.uzumaki_percount = 15;
-	this.maru_percount    = 75;
-	this.maru_shot_pertheta = 12;
 
 };
 Util.inherit(Spell, BaseSpell);
@@ -24,80 +14,71 @@ Util.inherit(Spell, BaseSpell);
 // 初期化
 Spell.prototype.init = function() {
 	BaseSpell.prototype.init.apply(this, arguments);
-
-	// まずは中央に移動
-	this.boss.setMoveTo(this.stage.width / 2, this.stage.height / 2);
 };
 
 Spell.prototype.runInSpellExecute = function() {
-	if(this.boss.isMoving()) return;
+	this.boss.is_show = false;
+	var count = this.frameCountStartedBySpellExec();
 
-	// 渦巻き弾
-	if(this.frame_count % this.uzumaki_percount === 0) {
-		this.uzumaki_shot1();
-		this.uzumaki_shot2();
-		this.game.playSound('boss_shot_small');
-	}
+	var x = 240;
+	var y = 100;
 
-	// 円形弾
-	if(this.frame_count % this.maru_percount === 0) {
-		// 自機狙い
-		this.aimedToChara();
-		for (var i=0; i< 360 / this.maru_shot_pertheta; i++) {
-			this.maru_shot();
-			this.maru_shot_theta += this.maru_shot_pertheta;
-		}
-		this.game.playSound('boss_shot_big');
-	}
-
-};
-
-Spell.prototype.uzumaki_shot1 = function() {
-	var x = this.boss.x;
-	var y = this.boss.y;
-	var r = this.r;
-
-	for(var i = 0; i < this.shot_thetas1.length; i++ ) {
-		var theta = this.shot_thetas1[i];
-
-		this.shot(Constant.BULLET_TINY_YELLOW, x, y, {r: r, theta: theta});
-		this.shot_thetas1[i] += this.add_shot_theta;
+	if(count % 75 === 0) {
+		this.shotCommonSense(count*2 % this.stage.width, -100);
 	}
 };
-Spell.prototype.uzumaki_shot2 = function() {
-	var x = this.boss.x;
-	var y = this.boss.y;
-	var r = this.r;
 
-	for(var i = 0; i < this.shot_thetas2.length; i++ ) {
-		var theta = this.shot_thetas2[i];
+Spell.prototype.shotCommonSense = function(x, y) {
+	var vec = {r: 4, theta: 90, ra: 0.1};
+	var i;
+	var mayuge_x = 44;
+	for (i = 0; i < 5; i++) {
+		// 左眉毛
+		this.shot(Constant.BULLET_BALL_LIMEGREEN, x - mayuge_x - 16 * i, y + 16 * (4-i), vec);
 
-		this.shot(Constant.BULLET_TINY_YELLOW, x, y, {r: r, theta: theta});
-		this.shot_thetas2[i] -= this.add_shot_theta;
+		// 右眉毛
+		this.shot(Constant.BULLET_BALL_LIMEGREEN, x + mayuge_x + 16 * i, y + 16 * (4-i), vec);
 	}
-};
-Spell.prototype.maru_shot = function() {
-	var x = this.boss.x;
-	var y = this.boss.y;
-	var theta = this.maru_shot_theta;
-	var r = this.r;
 
-	this.shot(Constant.BULLET_DOUBLEBALL_RED, x, y, {r: r, theta: theta});
-};
+	var eye_x = 76;
+	var eye_y = 120;
+	// 目
+	this.shot(Constant.BULLET_BIG_LIMEGREEN, x - eye_x, y + eye_y, vec); // type_id: 1
+	this.shot(Constant.BULLET_BIG_LIMEGREEN, x + eye_x, y + eye_y, vec); // type_id: 1
 
-// 自機狙いにする
-Spell.prototype.aimedToChara = function() {
-	// 自機
-	var character = this.stage.character;
+	var hoho_y = 180;
+	var hoho_x = 116;
 
-	var ax = character.x - this.boss.x;
-	var ay = character.y - this.boss.y;
+	for (i = 0; i < 4; i++) {
+		// 右ほほ
+		this.shot(Constant.BULLET_BALL_LIMEGREEN, x + hoho_x + (3-i)*8, y + hoho_y - 8 * (i%2 ? 0 : 1), vec);
+		// 左ほほ
+		this.shot(Constant.BULLET_BALL_LIMEGREEN, x - hoho_x - (3-i)*8, y + hoho_y - 8 * (i%2 ? 1 : 0), vec);
+	}
 
-	this.maru_shot_theta = Util.radianToTheta(Math.atan2(ay, ax));
+	var mouse_y = 190;
+	var offset = 12;
+
+	// 口(斜め線)
+	for (i = 0; i< 4; i++) {
+		this.shot(Constant.BULLET_BALL_LIMEGREEN, x - offset/2 - offset*i, y + mouse_y+offset*(4-i), vec);
+		this.shot(Constant.BULLET_BALL_LIMEGREEN, x + offset/2 + offset*i, y + mouse_y+offset*(4-i), vec);
+	}
+
+	// 口(上の線
+	var offset2 = 16;
+	this.shot(Constant.BULLET_BALL_LIMEGREEN, x, y + mouse_y, vec);
+	for (i = 0; i< 3; i++) {
+		this.shot(Constant.BULLET_BALL_LIMEGREEN, x-(16*(i+1)), y + mouse_y, vec);
+		this.shot(Constant.BULLET_BALL_LIMEGREEN, x+(16*(i+1)), y + mouse_y, vec);
+	}
 };
 
 Spell.prototype.name = function() { return "風符「天狗風」"; };
+Spell.prototype.charaImage = function() { return "merry_normal"; };
 
-Spell.prototype.charaImage = function() { return "aya_normal"; };
+// 初期 x, y 座標
+Spell.prototype.initX = function( ) { return 240; };
+Spell.prototype.initY = function( ) { return 100; };
 
 module.exports = Spell;
