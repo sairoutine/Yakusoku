47b10c19088124ff2d40c442aec4a020a1fee992
sairoutine <sairoutine@gmail.com>
2016-12-04 22:04:41 +0900

    add: spell1 of stage5

---
+++ src/js/spell/stage5/spell1.js
@@ -0,0 +1,91 @@
+'use strict';
+
+/* スペルカード */
+var BaseSpell = require('../base');
+var Util = require('../../util');
+var Constant = require('../../constant');
+
+var Spell = function(boss) {
+	BaseSpell.apply(this, arguments);
+};
+Util.inherit(Spell, BaseSpell);
+
+// 初期化
+Spell.prototype.init = function() {
+	BaseSpell.prototype.init.apply(this, arguments);
+
+	this.moveIndex = 0;
+	this.moveTo = [
+		{x: 200, y: 100},
+		{x: 300, y: 100},
+		{x: 400, y: 100},
+		{x: 300, y: 100},
+		{x: 200, y: 100},
+		{x: 100, y: 100},
+	];
+
+	this.shotCount = 0;
+};
+
+
+Spell.prototype.runInSpellExecute = function() {
+	this.boss.is_show = false;
+
+	if (!this.boss.isMoving() && this.shotCount >= 3) {
+		var move = this.moveTo[this.moveIndex];
+		this.boss.setMoveTo(move.x, move.y);
+
+		this.moveIndex++;
+
+		if(this.moveIndex > 5) {
+			this.moveIndex = 0;
+		}
+
+		this.shotCount = 0;
+	}
+
+	if(!this.boss.isMoving()) {
+		if(this.frame_count % 50 === 0){
+			this.shotCount++;
+
+			var r = 50;
+			var aimed_theta = this.calcThetaAimedToChara();
+
+			/* 交差 */
+			for (var i = 0; i < 20; i++) {
+				var theta = i * 18 + aimed_theta;
+
+				var type_id = Constant.BULLET_TINY_RED;
+				for (var j = 0; j < 10; j++) {
+					var r = 20 * 0.5*j;
+					var offset_x = r * Math.cos( Util.thetaToRadian( theta ) );
+					var offset_y = r * Math.sin( Util.thetaToRadian( theta ) );
+					theta += (i%2===0 ? -90 : 90);
+
+					this.shot(type_id, this.boss.x + offset_x, this.boss.y + offset_y, [
+						{ count: 0 , vector: {r: 0.5*j, theta: theta, ra: 0.05} },
+					]);
+				}
+			}
+		}
+	}
+
+	if(this.boss.vital < 10) {
+		this.boss.is_show = true;
+	}
+};
+
+Spell.prototype.name = function() { return "風符「天狗風」"; };
+Spell.prototype.charaImage = function() { return "aya_normal"; };
+
+Spell.prototype.initX = function() { return 100; };
+Spell.prototype.initY = function() { return 100; };
+
+Spell.prototype._getRandomValue = function( range ) {
+  var differ = range.max - range.min ;
+  return ((Math.random() * differ) | 0) + range.min ;
+} ;
+
+// 初期 x, y 座標
+
+module.exports = Spell;
