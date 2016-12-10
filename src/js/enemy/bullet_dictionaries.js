abafdee68dab345528c3eaa2ffd9cb977dc930ae
sairoutine <sairoutine@gmail.com>
2016-12-10 19:47:36 +0900

    add: bullet dictionary

--- src/js/enemy/bullet_dictionaries.js
+++ src/js/enemy/bullet_dictionaries.js
@@ -1,4 +1,5 @@
 'use strict';
+var Constant = require('../constant');
 
 var BulletDictionaries = [
 	[
@@ -31,7 +32,42 @@ var BulletDictionaries = [
 		{ vector: { r: 3, theta: 135, w: 0, ra: 0, wa: 0, raa: 0 } },
 		{ vector: { r: 3, theta: 225, w: 0, ra: 0, wa: 0, raa: 0 } },
 		{ vector: { r: 3, theta: 315, w: 0, ra: 0, wa: 0, raa: 0 } },
-	]
+	],
+	[
+		{
+			'type': Constant.BULLET_DOUBLEBALL_RED,
+			'vector': { 'r': 5, 'theta':  0, 'w': 0, 'ra': 0.1, 'wa': 0, 'raa': -0.01, 'rrange': { 'min': 2 } },
+		},
+		{
+			'type': Constant.BULLET_DOUBLEBALL_RED,
+			'vector': { 'r': 5, 'theta':  45, 'w': 0, 'ra': 0.1, 'wa': 0, 'raa': -0.01, 'rrange': { 'min': 2 } },
+		},
+		{
+			'type': Constant.BULLET_DOUBLEBALL_RED,
+			'vector': { 'r': 5, 'theta':  90, 'w': 0, 'ra': 0.1, 'wa': 0, 'raa': -0.01, 'rrange': { 'min': 2 } },
+		},
+		{
+			'type': Constant.BULLET_DOUBLEBALL_RED,
+			'vector': { 'r': 5, 'theta':  135, 'w': 0, 'ra': 0.1, 'wa': 0, 'raa': -0.01, 'rrange': { 'min': 2 } },
+		},
+		{
+			'type': Constant.BULLET_DOUBLEBALL_RED,
+			'vector': { 'r': 5, 'theta':  180, 'w': 0, 'ra': 0.1, 'wa': 0, 'raa': -0.01, 'rrange': { 'min': 2 } },
+		},
+		{
+			'type': Constant.BULLET_DOUBLEBALL_RED,
+			'vector': { 'r': 5, 'theta':  225, 'w': 0, 'ra': 0.1, 'wa': 0, 'raa': -0.01, 'rrange': { 'min': 2 } },
+		},
+		{
+			'type': Constant.BULLET_DOUBLEBALL_RED,
+			'vector': { 'r': 5, 'theta':  270, 'w': 0, 'ra': 0.1, 'wa': 0, 'raa': -0.01, 'rrange': { 'min': 2 } },
+		},
+		{
+			'type': Constant.BULLET_DOUBLEBALL_RED,
+			'vector': { 'r': 5, 'theta':  315, 'w': 0, 'ra': 0.1, 'wa': 0, 'raa': -0.01, 'rrange': { 'min': 2 } },
+		},
+
+	],
 ];
 
 module.exports = BulletDictionaries;
