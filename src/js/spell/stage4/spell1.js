f62231bdb2bb3ad4c52463f357cd1b7654c08830
sairoutine <sairoutine@gmail.com>
2016-12-26 00:45:40 +0900

    update: yukari's spell

--- src/js/spell/stage4/spell1.js
+++ src/js/spell/stage4/spell1.js
@@ -14,31 +14,30 @@ Util.inherit(Spell, BaseSpell);
 Spell.prototype.init = function() {
 	BaseSpell.prototype.init.apply(this, arguments);
 
-	this.shotCount = 0;
-
 	this.moveIndex = 0;
 	this.moveTo = [
-		{x: 300, y: 60},
-		{x: 360, y: 100},
-		{x: 300, y: 140},
-
-		{x: 240, y: 100},
-
-		{x: 180, y: 60},
-		{x: 120, y: 100},
-		{x: 180, y: 140},
-
-		{x: 240, y: 100},
+		{x: 100, y: 100},
+		{x: 400, y: 100},
+		{x: 100, y: 400},
+		{x: 400, y: 400},
 	];
+
+	this.shotCount = 0;
 
 };
 
 
 Spell.prototype.runInSpellExecute = function() {
-	// move
-	if (!this.boss.isMoving() && this.shotCount >= 10) {
+	if(this.boss.isMoving()) {
+		this.boss.is_show = false;
+	}
+	else {
+		this.boss.is_show = true;
+	}
+
+	if (!this.boss.isMoving() && this.shotCount >= 3) {
 		var move = this.moveTo[this.moveIndex];
-		this.boss.setMoveTo(move.x, move.y);
+		this.boss.setMoveTo(move.x, move.y, 100);
 
 		this.moveIndex++;
 
@@ -49,32 +48,41 @@ Spell.prototype.runInSpellExecute = function() {
 		this.shotCount = 0;
 	}
 
-	// shot
-	if(this.frame_count % 15 === 0) {
-		this.shotCount++;
+	if(!this.boss.isMoving()) {
+		if(this.frame_count % 50 === 0){
+			this.shotCount++;
 
-		var type_id = Constant.BULLET_KUNAI_PURPLE;
-		var r = 20;
-		var aimed_theta = this.calcThetaAimedToChara();
+			this.game.playSound('boss_shot_small');
+			var r = 50;
+			var aimed_theta = this.calcThetaAimedToChara();
 
-		/* 円形 */
-		for (var i = 0; i < 40; i++) {
-			var theta = i * 9 + aimed_theta;
-			var offset_x = r * Math.cos( Util.thetaToRadian( theta ) );
-			var offset_y = r * Math.sin( Util.thetaToRadian( theta ) );
+			/* 円形 */
+			for (var i = 0; i < 40; i++) {
+				var theta = i * 9 + aimed_theta;
+				var offset_x = r * Math.cos( Util.thetaToRadian( theta ) );
+				var offset_y = r * Math.sin( Util.thetaToRadian( theta ) );
 
-			this.shot(type_id, this.boss.x + offset_x, this.boss.y + offset_y, [
-				{ count: 0, vector: {r: 3, theta: theta} },
-			]);
+				var type_id = Constant.BULLET_KUNAI_PURPLE;
+				for (var j = 0; j < 10; j++) {
+					this.shot(type_id, this.boss.x + offset_x, this.boss.y + offset_y, [
+						{ count: 0 , vector: {r: 0, theta: theta} },
+						{ count: 25 , vector: {r: 3 + 0.4*j, theta: theta} },
+					]);
+				}
+			}
 		}
 	}
 };
 
-Spell.prototype.name = function() { return "罔両「無限呪縛」"; };
+Spell.prototype.onend = function() {
+	this.boss.is_show = true;
+};
+
+Spell.prototype.name = function() { return "境符「十二次元と十三次元の境界」"; };
 Spell.prototype.charaImage = function() { return "yukari_normal"; };
 
 // 初期 x, y 座標
-Spell.prototype.initX = function() { return 240; };
-Spell.prototype.initY = function() { return 100; };
+//Spell.prototype.initX = function() { return 240; };
+//Spell.prototype.initY = function() { return 100; };
 
 module.exports = Spell;
