e6d245f8d3962c86d7c863cd98205c41bb06e8de
sairoutine <sairoutine@gmail.com>
2016-12-26 01:31:46 +0900

    rename: lastboss's spell

--- src/js/spell/stage5/spell1.js
+++ src/js/spell/stage5/spell1.js
@@ -1,9 +1,10 @@
 'use strict';
 
 /* スペルカード */
-var BaseSpell = require('../base');
+var BaseSpell = require('../param_base');
 var Util = require('../../util');
 var Constant = require('../../constant');
+
 
 var Spell = function(boss) {
 	BaseSpell.apply(this, arguments);
@@ -13,74 +14,71 @@ Util.inherit(Spell, BaseSpell);
 // 初期化
 Spell.prototype.init = function() {
 	BaseSpell.prototype.init.apply(this, arguments);
-
-	this.moveIndex = 0;
-	this.moveTo = [
-		{x: 200, y: 100},
-		{x: 300, y: 100},
-		{x: 400, y: 100},
-		{x: 300, y: 100},
-		{x: 200, y: 100},
-		{x: 100, y: 100},
-	];
-
-	this.shotCount = 0;
 };
 
 
 Spell.prototype.runInSpellExecute = function() {
-
-	if (!this.boss.isMoving() && this.shotCount >= 3) {
-		var move = this.moveTo[this.moveIndex];
-		this.boss.setMoveTo(move.x, move.y);
-
-		this.moveIndex++;
-
-		if(this.moveIndex > 5) {
-			this.moveIndex = 0;
-		}
-
-		this.shotCount = 0;
-	}
-
-	if(!this.boss.isMoving()) {
-		if(this.frame_count % 50 === 0){
-			this.shotCount++;
-
-			var r = 50;
-			var aimed_theta = this.calcThetaAimedToChara();
-
-			/* 交差 */
-			for (var i = 0; i < 20; i++) {
-				var theta = i * 18 + aimed_theta;
-
-				var type_id = Constant.BULLET_TINY_RED;
-				for (var j = 0; j < 10; j++) {
-					var r = 20 * 0.5*j;
-					var offset_x = r * Math.cos( Util.thetaToRadian( theta ) );
-					var offset_y = r * Math.sin( Util.thetaToRadian( theta ) );
-					theta += (i%2===0 ? -90 : 90);
-
-					this.shot(type_id, this.boss.x + offset_x, this.boss.y + offset_y, [
-						{ count: 0 , vector: {r: 0.5*j, theta: theta, ra: 0.05} },
-					]);
-				}
-			}
-		}
-	}
+	BaseSpell.prototype.runInSpellExecute.apply(this, arguments);
 };
 
 Spell.prototype.name = function() { return "?????????"; };
 Spell.prototype.charaImage = function() { return "merry_furious"; };
 
-Spell.prototype.initX = function() { return 100; };
-Spell.prototype.initY = function() { return 100; };
-
-Spell.prototype._getRandomValue = function( range ) {
-  var differ = range.max - range.min ;
-  return ((Math.random() * differ) | 0) + range.min ;
-} ;
-
 // 初期 x, y 座標
+Spell.prototype.initX = function( ) { return this.stage.width/2; };
+Spell.prototype.initY = function( ) { return this.stage.height/2; };
+
+Spell.prototype.baseCount = function( ) { return 465; };
+
+Spell.prototype.shotParam = function( ) {
+	var r_num        = 20; // 弾幕の円の半径(大きいほど外側への弾の数が増えていく)
+	var shotParam = [];
+
+	for(var i = 0; i<r_num; i++) {
+		var type = i%2 ? Constant.BULLET_TINY_LIMEGREEN : Constant.BULLET_TINY_BLUE;
+
+		shotParam.push({
+			'bullet':i, 'type': type, 'count': [ 1 ]
+		});
+	}
+
+	return shotParam;
+};
+
+Spell.prototype.moveParam = function( ) {
+	return [];
+};
+
+Spell.prototype.bulletDictionary = function( ) {
+	var r_space      = 17; // 弾幕の円の半径(小さいほど外側に行く方の弾の間隔が短くなっていく)
+	var r_num        = 20; // 弾幕の円の半径(大きいほど外側への弾の数が増えていく)
+	var density      = 48; // 弾と隣の弾の間隔
+	var spread_speed = 6;  // 弾を撒く速さ
+
+	var BulletDictionaries = [];
+
+	for (var j = 1; j <= r_num; j++) {
+		var array = [];
+		var r = r_space * j;
+		for( var k = 0; k < density; k++ ) {
+			var count = k;
+			var theta = ( ( k * 360/density ) + 450 ) % 360;
+			var v = { 'x': r * Math.cos( Util.thetaToRadian( theta ) ),
+				'y': r * Math.sin( Util.thetaToRadian( theta ) ),
+				'count': count + j * density/spread_speed,
+				'vector': [
+					{ 'count': 0, 'vector': { 'r': 0, 'theta': theta, } },
+					{ 'count': (density/spread_speed-k)+(r_num-j)*density/spread_speed + 100, 'vector': { 'r':2.5, theta: theta + 180} },
+				],
+			} ;
+			array.push( v ) ;
+		}
+
+
+		BulletDictionaries.push(array);
+	}
+
+	return BulletDictionaries;
+};
 
 module.exports = Spell;
