7e14d49ca70957fa03cc115a8e8b37f0ff4aa2f5
sairoutine <sairoutine@gmail.com>
2017-01-11 01:03:19 +0900

    compile js

--- public/js/main.js
+++ public/js/main.js
@@ -216,9 +216,9 @@ module.exports = MersenneTwister;
 var Constant = require('./constant');
 
 var Config = {
-	DEBUG: true,
+	DEBUG: false,
 	//DEBUG_SCENE: Constant.STAGE_SCENE,
-	//DEBUG_STATE: Constant.BOSS_STATE,
+	//DEBUG_STATE: Constant.GAMEOVER_STATE,
 	//DEBUG_STAGE: 4,
 	//DEBUG_SPELL: 2,
 	//DEBUG_MUSIC_OFF: true,
@@ -9679,7 +9679,7 @@ Scene.prototype.notifyCharacterDead = function() {
 };
 
 // ゲームオーバーのリザルト終了後
-Scene.prototype.notifyGameOverEnd = function() {
+Scene.prototype.notifyRetry = function() {
 	// コンティニューしたらスコアを半分にする
 	this.score = Math.floor(this.score / 2);
 
@@ -10210,15 +10210,105 @@ module.exports = ResultState;
 
 var BaseState = require('./result_base');
 var Util = require('../../../util');
+var Constant = require('../../../constant');
 
 var ResultState = function(stage) {
 	BaseState.apply(this, arguments);
 };
 Util.inherit(ResultState, BaseState);
 
+ResultState.prototype.init = function(){
+	BaseState.prototype.init.apply(this, arguments);
+
+	this.selectIndex = 0;
+};
+
+ResultState.prototype.run = function(){
+	BaseState.prototype.run.apply(this, arguments);
+	if(!this.isInTransition()) {
+		if(this.game.isKeyPush(Constant.BUTTON_UP)) {
+			this.game.playSound('select');
+			this.selectIndex = 0;
+		}
+		else if(this.game.isKeyPush(Constant.BUTTON_DOWN)) {
+			this.game.playSound('select');
+			this.selectIndex = 1;
+		}
+	}
+};
+
+var RESULT_TRANSITION_COUNT = 100;
+// スコア結果画面のオーバーライド
+ResultState.prototype._showScoreWindow = function(){
+	var ctx = this.game.surface;
+
+	ctx.save();
+
+	var alpha = 1.0 ;
+	if(this.frame_count < RESULT_TRANSITION_COUNT) {
+		alpha = this.frame_count / RESULT_TRANSITION_COUNT;
+	}
+	else {
+		alpha = 1.0;
+	}
+
+	ctx.fillStyle = 'rgb( 0, 0, 0 )' ;
+	ctx.globalAlpha = alpha * 0.5; // タイトル背景黒は半透明
+	ctx.fillRect(0, 140, this.stage.width, 180);
+
+	ctx.globalAlpha = alpha; // 文字を表示するので戻す
+
+	ctx.fillStyle = 'rgb( 255, 255, 255 )';
+	ctx.textAlign = 'center';
+	ctx.font = "18px 'Migu'" ;
+	ctx.fillText(this.resultName(), this.stage.width/2, 180);
+
+
+	ctx.fillStyle = 'rgb( 255, 255, 255 )';
+	ctx.textAlign = 'left';
+	ctx.font = "16px 'Migu'" ;
+	ctx.fillText( 'Result', 100, 210);
+	ctx.textAlign = 'right' ;
+	ctx.fillText('Score: ' + this.stage.score, 380, 210);
+	// ステージ名とタイトルの間の白い棒線
+	ctx.fillRect(100, 225, 280, 1);
+
+	ctx.textAlign = 'center';
+
+	if(this.selectIndex === 0) {
+		ctx.globalAlpha = 1.0 * alpha;
+	}
+	else {
+		ctx.globalAlpha = 0.2 * alpha;
+	}
+
+	ctx.fillText('Continue', this.stage.width/2, 265);
+
+	if(this.selectIndex === 0) {
+		ctx.globalAlpha = 0.2 * alpha;
+	}
+	else {
+		ctx.globalAlpha = 1.0 * alpha;
+	}
+
+	ctx.fillText('Quit', this.stage.width/2, 295);
+
+	ctx.restore();
+};
+
+
+
+
 // リザルト画面が終了した
 ResultState.prototype.notifyResultEnd = function () {
-	this.stage.notifyGameOverEnd();
+	if(this.selectIndex === 0) {
+		// Continue
+		this.stage.notifyRetry();
+	}
+	else if(this.selectIndex === 1) {
+		// Quit
+		this.stage.notifyStageQuit();
+	}
 };
 
 ResultState.prototype.resultName = function(){
@@ -10229,7 +10319,7 @@ ResultState.prototype.resultName = function(){
 
 module.exports = ResultState;
 
-},{"../../../util":94,"./result_base":54}],57:[function(require,module,exports){
+},{"../../../constant":3,"../../../util":94,"./result_base":54}],57:[function(require,module,exports){
 'use strict';
 
 /* 道中 最初のタイトル表示 */
